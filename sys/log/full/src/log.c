5049a2eeb3968a7f2d3857f16b009dc59200e245
Saul Pwanson <saul@cionic.com>
2019-10-04 18:03:09 -0700

    [log] remove log_chk_type/assert

--- sys/log/full/src/log.c
+++ sys/log/full/src/log.c
@@ -392,28 +392,6 @@ log_set_append_cb(struct log *log, log_append_cb *cb)
 }
 
 static int
-log_chk_type(uint8_t etype)
-{
-    int rc;
-
-    rc = OS_OK;
-
-#if MYNEWT_VAL(LOG_VERSION) > 2
-    switch(etype) {
-        case LOG_ETYPE_STRING:
-        case LOG_ETYPE_BINARY:
-        case LOG_ETYPE_CBOR:
-            break;
-        default:
-            rc = OS_ERROR;
-            break;
-    }
-#endif
-
-    return rc;
-}
-
-static int
 log_chk_max_entry_len(struct log *log, uint16_t len)
 {
     int rc;
@@ -439,9 +417,6 @@ log_append_prepare(struct log *log, uint8_t module, uint8_t level,
     uint32_t idx;
 
     rc = 0;
-
-    rc = log_chk_type(etype);
-    assert(rc == OS_OK);
 
     if (log->l_name == NULL || log->l_log == NULL) {
         rc = -1;
