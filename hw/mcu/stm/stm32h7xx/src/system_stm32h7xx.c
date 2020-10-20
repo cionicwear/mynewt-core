444cad3442b2d4c990ae482d9b47ad39e3f46a16
Jocelyn <jocemass@gmail.com>
2020-10-20 13:36:56 +0200

    Fix build issue

--- hw/mcu/stm/stm32h7xx/src/system_stm32h7xx.c
+++ hw/mcu/stm/stm32h7xx/src/system_stm32h7xx.c
@@ -116,6 +116,11 @@
   uint32_t SystemD2Clock = 64000000;
   const  uint8_t D1CorePrescTable[16] = {0, 0, 0, 0, 1, 2, 3, 4, 1, 2, 3, 4, 6, 7, 8, 9};
 
+/*
+ * XXX BSP specific
+ */
+void SystemClock_Config(void);
+
 /**
   * @}
   */
