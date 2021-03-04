e6ab06e01f4c84d4f6d518b8c9784f1b06e95941
Jocelyn <jocemass@gmail.com>
2021-03-04 18:55:41 +0100

    Enable ADC peripheral

--- hw/mcu/stm/stm32h7xx/src/clock_stm32h7xx.c
+++ hw/mcu/stm/stm32h7xx/src/clock_stm32h7xx.c
@@ -236,7 +236,7 @@ SystemClock_Config(void)
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_UART4
                               |RCC_PERIPHCLK_USART6|RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_USART3
                               |RCC_PERIPHCLK_UART5|RCC_PERIPHCLK_UART7
-                              |RCC_PERIPHCLK_UART8;
+                              |RCC_PERIPHCLK_UART8 | RCC_PERIPHCLK_ADC;
     PeriphClkInitStruct.PLL2.PLL2M = MYNEWT_VAL(STM32_CLOCK_PLL2_PLLM);
     PeriphClkInitStruct.PLL2.PLL2N = MYNEWT_VAL(STM32_CLOCK_PLL2_PLLN);
     PeriphClkInitStruct.PLL2.PLL2P = MYNEWT_VAL(STM32_CLOCK_PLL2_PLLP);
